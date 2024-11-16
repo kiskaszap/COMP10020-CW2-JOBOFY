9b6f933b228704b2db094eb68028625e50348531
Balint Kaszap-Nagy <kaszap.nagy.balint@gmail.com>
2024-11-16 14:41:53 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 // Global middlewares
 app.use(cors());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://comp10020-cw2-jobofy.onrender.com", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
